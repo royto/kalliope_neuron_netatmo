f2e8375ae7d8b9d8052685a65872d44299b35288
royto <royto81+github@gmail.com>
2018-11-04 22:34:52 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,211 @@
+# Netatmo
+
+## Synopsis
+
+This neuron allows to control your netatmo devices.
+*Only energy devices are currently supported*
+
+You'll need a netatmo account and developer id to use the module.
+See Getting the Netatmo developper id on how to do it.
+
+## Installation
+
+```bash
+kalliope install --git-url https://github.com/royto/kalliope_neuron_netatmo.git
+```
+
+## CHANGE_MODE
+
+Allows to change thermostat mode (schedule, away or frost guard)
+
+### Options
+
+| parameter   | required | type   | default | choices    | comment                              |
+|-------------|----------|--------|---------|------------|--------------------------------------|
+| homeId    | YES      | String | None    |            | id of the home      |
+| thermMode | YES      | String | None    |  schedule or away or hg | The mode to set |
+
+#### Return Values
+
+No return Yet
+
+| Name    | Description                                                          | Type   | sample        |
+|---------|----------------------------------------------------------------------|--------|---------------|
+
+#### Synapses example
+
+``` yml
+  - name: "netatmo-therm-away"
+    signals:
+      - order: "Set thermostat mode to away"
+    neurons:
+      - netatmo:
+          clientId: "XXX"
+          clientSecret: "XXX"
+          username: "XXX@gmail.com"
+          password: "XXX"
+          action: "CHANGE_MODE"
+          homeId: "XXX"
+          thermMode: "away"
+          say_template: 
+            - "Thermostat set to mode away"
+```
+
+## GET_STATUS
+
+Allows to get thermostat status
+
+### Options
+
+| parameter   | required | type   | default | choices    | comment                              |
+|-------------|----------|--------|---------|------------|--------------------------------------|
+| homeId    | YES      | String | None    |            | id of the home      |
+
+#### Return Values
+
+Home status.
+
+| Name    | Description                                                          | Type   | sample        |
+|---------|----------------------------------------------------------------------|--------|---------------|
+| rooms | list of rooms | dict of Rooms | |
+
+##### Rooms
+
+| Name    | Description                                                          | Type   | sample        |
+|---------|----------------------------------------------------------------------|--------|---------------|
+|id|id of the room|Number| 12345|
+|name|Name of the room|String| Kitchen |
+|reachable|False if none of the module of the room are reachable|Boolean|True|
+|currentTemp|Current temperature of the room|float|21
+|mode|Current Mode (manual, max, off, schedule, away, hg)|String| Schedule|
+|wantedTemp|Wanted temperature in the room|float| 22.5|
+
+#### Synapses example
+
+``` yml
+ - name: "netatmo-therm"
+    signals:
+      - order: "What is the temperature in the house"
+    neurons:
+      - netatmo:
+          clientId: "XXX"
+          clientSecret: "XXX"
+          username: "XXX@XXX.com"
+          password: "XXX"
+          action: "GET_STATUS"
+          homeId: "XXX"
+          file_template: "templates/home_status.j2"
+```
+
+The template defined in the templates/home_status.j2
+
+```jinja2
+{% for room in rooms %}
+    {{ room['name'] }} is at {{ room['currentTemp'] }} celcius degree.
+    Wnated tempetaure is {{ room['wantedTemp'] }}.
+{% endfor %}
+```
+
+## SET_TEMP
+
+Allows to change temperature of a rooom
+
+### Options
+
+| parameter   | required | type   | default | choices    | comment                              |
+|-------------|----------|--------|---------|------------|--------------------------------------|
+| homeId    | YES      | String | None    |            | id of the home      |
+| roomId    | YES      | String | None    |            | id of the room      |
+| temp     | YES      | float | None    |            | the temperature to set      |
+
+#### Return Values
+
+No value returned.
+
+#### Synapses example
+
+``` yml
+  - name: "netatmo-set-temp"
+    signals:
+      - order: "Set temperature to 20 in the living room"
+    neurons:
+      - netatmo:
+          clientId: "XXX"
+          clientSecret: "XXX"
+          username: "XXX"
+          password: "XXX"
+          action: "SET_TEMP"
+          homeId: "XXX"
+          temp: 20
+          say_template: "Temperature changed"
+```
+
+## CANCEL_SET_TEMP
+
+Cancel the temperature set for a room and use scheduled temperature.
+
+### Options
+
+| parameter   | required | type   | default | choices    | comment                              |
+|-------------|----------|--------|---------|------------|--------------------------------------|
+| homeId    | YES      | String | None    |            | id of the home      |
+| roomId    | YES      | String | None    |            | id of the room      |
+
+#### Return Values
+
+No value returned.
+
+#### Synapses example
+
+``` yml
+  - name: "netatmo-cancel-temp"
+    signals:
+      - order: "Cancel boost mode in the living room"
+    neurons:
+      - netatmo:
+          clientId: "XXX"
+          clientSecret: "XXX"
+          username: "XXX"
+          password: "XXX"
+          action: "CANCEL_SET_TEMP"
+          homeId: "XXXX"
+          roomId: "XXX"
+          say_template: "Boost mode canceled"
+```
+
+## SWITCH_SCHEDULE
+
+ALlows to switch the selected scheduled.
+
+### Options
+
+| parameter   | required | type   | default | choices    | comment                              |
+|-------------|----------|--------|---------|------------|--------------------------------------|
+| homeId    | YES      | String | None    |            | id of the home      |
+| scheduleId    | YES      | String | None    |            | id of the schedule      |
+
+#### Return Values
+
+No value returned.
+
+#### Synapses example
+
+``` yml
+  - name: "netatmo-switch-schedule"
+    signals:
+      - order: "Switch to schedule holidays"
+    neurons:
+      - netatmo:
+          clientId: "XXX"
+          clientSecret: "XXX"
+          username: "XXX"
+          password: "XXX"
+          action: "SWITCH_SCHEDULE"
+          homeId: "XXXX"
+          scheduleId: "XXX"
+          say_template: "Scheduled changed"
+```
+
+## Getting the Netatmo developper id
+
+Connect to the Netatmo connect website https://dev.netatmo.com/myaccount/ and create an app. Save the clientId and clientSecret automatically generated by Netatmo.
---
+++ netatmo.py
@@ -0,0 +1,209 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import requests, json
+
+from kalliope.core.NeuronModule import (NeuronModule,
+                                        MissingParameterException,
+                                        InvalidParameterException)
+
+logging.basicConfig()
+logger = logging.getLogger("kalliope")
+
+_BASE_URL = "https://api.netatmo.com/"
+_AUTH_REQ = _BASE_URL + "oauth2/token"
+_SET_THERM_MODE = _BASE_URL + "/api/setthermmode"
+_HOME_STATUS = _BASE_URL + "api/homestatus"
+_HOME_DATA = _BASE_URL + "api/homesdata"
+_SET_ROOM_THERMPOINT = _BASE_URL + "api/setroomthermpoint"
+_SWITCH_HOME_SCHEDULE = _BASE_URL + "api/switchhomeschedule"
+
+THERMMODE = ("schedule", "away", "hg")
+
+class Netatmo(NeuronModule):
+
+    """
+    Class used to interact with netatmo system
+    """
+    def __init__(self, **kwargs):
+
+        super(Netatmo, self).__init__(**kwargs)
+
+        # parameters
+        self.username = kwargs.get('username', None)
+        self.password = kwargs.get('password', None)
+        self.clientId = kwargs.get('clientId', None)
+        self.clientSecret = kwargs.get('clientSecret', None)
+        self.action = kwargs.get('action', None)
+        self.homeId = kwargs.get('homeId', None)
+        self.roomId = kwargs.get('roomId', None)
+        self.roomName = kwargs.get('roomName', None)
+        self.thermMode = kwargs.get('thermoMode', None)
+        self.temp = kwargs.get('temperature', None)
+        self.scheduleId = kwargs.get('scheduleId', None)
+
+        logger.debug("Netatmo launch %s", self.username)
+
+        # check parameters
+        if self._is_parameters_ok():
+
+            scope="read_thermostat write_thermostat" 
+            payload = {
+                "grant_type" : "password",
+                "client_id" : self.clientId,
+                "client_secret" : self.clientSecret,
+                "username" : self.username,
+                "password" : self.password,
+                "scope" : scope
+            }
+            headers = { 'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+
+            logger.debug(payload)
+            response = requests.post(_AUTH_REQ, headers=headers, data=payload)
+
+            content = response.json()
+            self._accessToken = content['access_token']
+            self.refreshToken = content['refresh_token']
+
+            logger.debug(self._accessToken)
+
+            if self.action == "GET_STATUS":
+                self.homeStatus()
+            elif self.action == "SET_TEMP":
+                self.changeRoomTemp()
+            elif self.action == "CANCEL_SET_TEMP":
+                self.cancelBoostMode()
+            elif self.action == "CHANGE_MODE":
+                self.changeMode()
+            elif self.action == "SWITCH_SCHEDULE":
+                self.switchSchedule()
+
+    def changeMode(self):
+        #thermMode schedule / away / hg
+        if self.thermMode not in THERMMODE:
+            raise InvalidParameterException("Invalid Therm mode")
+
+        headers = self.getAuthorizedHeader()
+        params = {
+            "home_id": self.homeId,
+            "mode" : self.thermMode
+        }
+        response = requests.post(_SET_THERM_MODE, headers=headers, params=params)
+
+    def homeData(self):
+        headers = self.getAuthorizedHeader()
+
+        response = requests.get(_HOME_DATA, headers=headers)
+
+        content = response.json()
+        return content["body"]["homes"][0]
+
+    def homeStatus(self):
+        headers = self.getAuthorizedHeader()
+
+        params = {
+            "home_id": self.homeId
+        }
+
+        homeData = self.homeData()
+        
+        response = requests.get(_HOME_STATUS, headers=headers, params=params)
+
+        result = dict()
+        result["rooms"] = []
+
+        content = response.json()
+        rooms = content["body"]["home"]["rooms"]
+        for room in rooms:
+            roomInfo = dict()
+            roomInfo["id"] = room["id"]
+            roomInfo["name"] = self._find_room_name_by_id(homeData, room["id"])
+            roomInfo["reachable"] = room["reachable"]
+            roomInfo["currentTemp"] = room["therm_measured_temperature"]
+            roomInfo["mode"] = room["therm_setpoint_mode"]
+            roomInfo["wantedTemp"] = room["therm_setpoint_temperature"]
+            result["rooms"].append(roomInfo)
+
+        self.say(result)
+
+    def changeRoomTemp(self):
+        logger.debug("changeRoomTemp to temp %s", self.temp)
+
+        if self.roomName is not None and self.roomId is None:
+            homeData = self.homeData()
+            self.roomId = self._find_room_id_by_Name(homeData, self.roomName)
+            if self.roomId is None: 
+                raise InvalidParameterException("room {} not found".format(self.roomName))
+            logger.debug("id of the room %s is %s", self.roomName, self.roomId)
+
+        params = {
+            "home_id": self.homeId,
+            "room_id": self.roomId,
+            "mode": "manual",
+            "temp": int(self.temp)
+        }
+        headers = self.getAuthorizedHeader()
+        response = requests.post(_SET_ROOM_THERMPOINT, headers=headers, params=params)
+
+        logger.debug(response.json())
+
+    def cancelBoostMode(self):
+        params = {
+            "home_id": self.homeId,
+            "room_id": self.roomId,
+            "mode": "home"
+        }
+        headers = self.getAuthorizedHeader()
+        response = requests.post(_SET_ROOM_THERMPOINT, headers=headers, params=params)
+
+        result = dict()
+        logger.debug(response.json())
+        result["ok"] = response.json()["status"]
+        self.say(result)
+        
+    def switchSchedule(self):
+        params = {
+            "home_id": self.homeId,
+            "schedule_id": self.scheduleId,
+        }
+        headers = self.getAuthorizedHeader()
+        response = requests.post(_SWITCH_HOME_SCHEDULE, headers=headers, params=params)
+
+    def getAuthorizedHeader(self): 
+        return {
+            "Authorization" :  "Bearer " + self._accessToken
+        }
+
+    def _find_room_name_by_id(self, homeData, roomId):
+        room = next((room for room in homeData["rooms"] if room["id"].lower() == roomId.lower()), None)
+        return None if room is None else room["name"]
+    
+    def _find_room_id_by_Name(self, homeData, roomName):
+        room = next((room for room in homeData["rooms"] if room["name"].lower() == roomName.lower()), None)
+        return None if room is None else room["id"]
+
+    def _find_schedule_id_by_Name(self, homeData, scheduleName):
+        schedule = next((schedule for schedule in homeData["schedules"] if schedule["name"].lower() == scheduleName.lower()), None)
+        return None if schedule is None else schedule["id"]
+
+    def _is_parameters_ok(self):
+        """
+        Check if received parameters are ok to perform operations in the neuron.
+        :return: True if parameters are ok, raise an exception otherwise.
+
+        .. raises:: MissingParameterException, InvalidParameterException
+        """
+        if self.username is None:
+            raise MissingParameterException("Netatmo needs a username")
+        if self.password is None:
+            raise MissingParameterException("Netatmo needs a password")
+        if self.clientId is None:
+            raise MissingParameterException("Netatmo needs a clientId")
+        if self.clientSecret is None:
+            raise MissingParameterException("Netatmo needs a clientSecret")
+        if self.action is None:
+            raise MissingParameterException("Netatmo needs an action")    
+        if self.homeId is None:
+            raise MissingParameterException("Netatmo needs a homeId")    
+        return True
+
